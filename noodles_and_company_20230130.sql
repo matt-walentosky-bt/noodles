9ed15398347956d810ded7046707bad4d62cf083
matt-walentosky-bt <94004986+matt-walentosky-bt@users.noreply.github.com>
2023-02-20 11:10:00 -0500

    Update noodles_and_company_20230130.sql

--- noodles_and_company_20230130.sql
+++ noodles_and_company_20230130.sql
@@ -1,267 +1,71 @@
-/*
-Query created by Matt Walentosky: 2023-01-30
-Purpose is to create table that can be used to measure effectiveness of product recomender
-Final table has format:
 
-user_pseudo_id,
-user_id,
-ga_session_id,
-event_date,
-min_timestamp,
-add_to_cart,
-event_name,
-item_name,
-cart_state
-*/
-
-with view_item_list as 
-(
-  select 
-  user_pseudo_id,
-  user_id,
-  (select value.int_value from unnest(event_params) where key = "ga_session_id") as ga_session_id,
-  event_date,
-  event_timestamp,
-  event_name,
-  items.item_name as item_name,
-  items.item_list_index as item_number
-  FROM `cool-arch-249617.analytics_207839834.events_20230129`, unnest(items) items
-  where event_name in ('view_item_list')
-  order by ga_session_id, event_timestamp
+--Get minimum recomendation time for each user, date, item_name, item_id
+WITH minimum_rec_gb as (
+  SELECT 
+    user_pseudo_id,
+    event_date,
+    (select value.string_value from unnest(event_params) where key = "basket_id") as basket_id,
+    items.item_name,
+    items.item_id,
+    items.item_category,
+    items.price as rec_price,
+    items.item_category as item_category,
+    items.item_list_name,
+    MIN(event_timestamp) as rec_timestamp
+  FROM `cool-arch-249617.analytics_207839834.events_*`, UNNEST(items) as items
+  --Must specify web
+  WHERE platform = 'WEB'
+  --Pull from 2 days back to 32 days back
+  AND _TABLE_SUFFIX BETWEEN FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 3 DAY))
+  AND FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
+  AND (SELECT value.string_value from UNNEST(event_params) WHERE key = 'page_location') LIKE '%my-bag%'
+  AND event_name = 'view_item_list'
+  GROUP BY 1,2,3,4,5,6,7,8,9
 ),
 
---build query to find items added to cart
-add_to_cart as 
-(
-  select 
-  user_pseudo_id,
-  user_id,
-  (select value.int_value from unnest(event_params) where key = "ga_session_id") as ga_session_id,
-  event_date,
-  event_timestamp,
-  event_name,
-  items.item_name as item_name,
-  items.item_list_index as item_number
-  FROM `cool-arch-249617.analytics_207839834.events_20230129`, unnest(items) items
-  where event_name in ('add_to_cart')
-  order by ga_session_id, event_timestamp
-),
---build query to find purchased items
-purchase as  
-(
-  select 
-  user_pseudo_id,
-  user_id,
-  (select value.int_value from unnest(event_params) where key = "ga_session_id") as ga_session_id,
-  event_date,
-  event_timestamp,
-  event_name,
-  items.item_name as item_name,
-  items.item_list_index as item_number
-  FROM `cool-arch-249617.analytics_207839834.events_20230129`, unnest(items) items
-  where event_name in ('purchase')
-  order by ga_session_id, event_timestamp
+--Get minimum add to cart time for each user, date, item_name, item_id
+min_atc_gb as (
+  SELECT 
+    user_pseudo_id,
+    event_date,
+    items.item_name,
+    items.item_id,
+    items.price as atc_price,
+    MIN(event_timestamp) as atc_timestamp
+  FROM `cool-arch-249617.analytics_207839834.events_*`, UNNEST(items) as items
+  WHERE platform = 'WEB'
+  AND _TABLE_SUFFIX BETWEEN FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 3 DAY))
+  AND FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
+  AND event_name = 'add_to_cart'
+  GROUP BY 1,2,3,4, 5
 ),
 
---union together data sources
-union_data as 
-(
- select * from view_item_list
- union all
- select * from add_to_cart
- union all
- select * from purchase
+purchases as (
+  SELECT 
+    user_pseudo_id,
+    event_date,
+    items.item_name,
+    items.item_id,
+    items.price as purchase_price,
+    MIN(event_timestamp) as purchase_timestamp
+  FROM `cool-arch-249617.analytics_207839834.events_*`, UNNEST(items) as items
+  WHERE platform = 'WEB'
+  AND _TABLE_SUFFIX BETWEEN FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(),INTERVAL 3 DAY))
+  AND FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
+  AND event_name = 'purchase'
+  GROUP BY 1,2,3,4,5
 ),
 
---create seperate variable for add_to_cart
-format_01 as 
+joined_data as 
 (
-select 
-user_pseudo_id,
-user_id,
-ga_session_id,
-event_date,
-event_timestamp,
-event_name,
-item_name,
-item_number,
-case when event_name = 'add_to_cart' then item_name else NULL END AS `add_to_cart`,
-case when event_name = 'purchase' then item_name else NULL END AS `purchase`
-from 
-union_data
-),
+SELECT 
+  *,
+  CASE WHEN rec_timestamp < atc_timestamp THEN 1 ELSE 0 END AS rec_atc,
+  CASE WHEN rec_timestamp < purchase_timestamp THEN 1 ELSE 0 END AS rec_purchase
 
---create string_agg variable
-format_02 as 
-(
-select 
-user_pseudo_id,
-user_id,
-ga_session_id,
-event_date,
-event_timestamp,
-add_to_cart,
-purchase,
-event_name,
-item_name,
-item_number,
-STRING_AGG(add_to_cart) over (partition by ga_session_id order by event_timestamp) as cart_state,
-STRING_AGG(purchase) over (partition by ga_session_id order by event_timestamp) as purchase_state
-from
-format_01
-group by
-user_pseudo_id, user_id, ga_session_id, 
-event_name, item_name, item_number,
-event_date, event_timestamp, add_to_cart, purchase
-order by 
-user_pseudo_id,
-ga_session_id,
-event_timestamp
-),
-
-min_view_item as
-(
-  select 
-user_pseudo_id,
-user_id,
-ga_session_id,
-event_date,
-event_name,
-item_name,
-cart_state, 
-NULL AS add_to_cart,
-min(event_timestamp) as event_timestamp
-from
-format_02
-where event_name = 'view_item_list'
-group by 
-user_pseudo_id,
-user_id,
-ga_session_id,
-event_date,
-event_name,
-item_name,
-cart_state
-order by
-user_pseudo_id,
-user_id,
-ga_session_id,
-event_date,
-event_timestamp
-),
-
-final_purchase_state2 as 
-(
-select
-distinct
-max_state.user_pseudo_id,
-max_state.user_id,
-max_state.ga_session_id,
-max_state.event_date,
-max_state.event_timestamp,
-format_02.purchase_state as final_purchase_state
-from
-(
-select 
-user_pseudo_id,
-user_id,
-ga_session_id,
-event_date,
-max(event_timestamp) as event_timestamp
-from
-format_02  
-group by
-user_pseudo_id,
-user_id,
-ga_session_id,
-event_date
-) max_state
-inner join
-format_02
-on 
-max_state.user_pseudo_id = format_02.user_pseudo_id and
-max_state.user_id = format_02.user_id and
-max_state.ga_session_id = format_02.ga_session_id and
-max_state.event_date = format_02.event_date and 
-max_state.event_timestamp = format_02.event_timestamp
-),
-
-final_cart_state2 as 
-(
-select
-distinct
-max_state.user_pseudo_id,
-max_state.user_id,
-max_state.ga_session_id,
-max_state.event_date,
-max_state.event_timestamp,
-format_02.cart_state as final_cart_state
-from
-(
-select 
-user_pseudo_id,
-user_id,
-ga_session_id,
-event_date,
-max(event_timestamp) as event_timestamp
-from
-format_02  
-group by
-user_pseudo_id,
-user_id,
-ga_session_id,
-event_date
-) max_state
-inner join
-format_02
-on 
-max_state.user_pseudo_id = format_02.user_pseudo_id and
-max_state.user_id = format_02.user_id and
-max_state.ga_session_id = format_02.ga_session_id and
-max_state.event_date = format_02.event_date and 
-max_state.event_timestamp = format_02.event_timestamp
-),
-
-final_join as
-(
-  select
-format_02.user_pseudo_id,
-format_02.user_id,
-format_02.ga_session_id,
-format_02.event_date,
-format_02.event_timestamp,
-format_02.item_name,
-format_02.cart_state,
-final_cart_state2.final_cart_state,
-final_purchase_state2.final_purchase_state
-from
-(select * from format_02 where event_name  = 'view_item_list') format_02
-left join
-final_cart_state2
-on
-final_cart_state2.user_pseudo_id = format_02.user_pseudo_id and
-final_cart_state2.user_id = format_02.user_id and
-final_cart_state2.ga_session_id = format_02.ga_session_id and
-final_cart_state2.event_date = format_02.event_date 
-left join
-final_purchase_state2
-on
-final_purchase_state2.user_pseudo_id = format_02.user_pseudo_id and
-final_purchase_state2.user_id = format_02.user_id and
-final_purchase_state2.ga_session_id = format_02.ga_session_id and
-final_purchase_state2.event_date = format_02.event_date 
+FROM minimum_rec_gb
+LEFT JOIN min_atc_gb USING(user_pseudo_id, event_date, item_name, item_id)
+LEFT JOIN purchases USING(user_pseudo_id, event_date, item_name, item_id)
 )
 
-select 
-user_pseudo_id,
-user_id,
-ga_session_id,
-event_date,
-event_timestamp,
-item_name,
-cart_state,
-final_cart_state,
-final_purchase_state,
-case when REGEXP_CONTAINS(`final_cart_state`,item_name) = TRUE then 1 else 0 end as a2c_status,
-case when REGEXP_CONTAINS(`final_purchase_state`,item_name) = TRUE then 1 else 0 end as purchase_status
-from final_join order by user_pseudo_id, user_id, ga_session_id, event_date, event_timestamp;
+select * from joined_data;
